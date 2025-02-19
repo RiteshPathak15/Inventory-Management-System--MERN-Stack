744e712f182fcbf92da5cf86bc9488254c62b746
Ritesh Pathak <pathakritesh982@gmail.com>
2025-02-19 10:16:30 +0530

    login

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
-dotenv.config()
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       // '/api': 'http://localhost:5000',
-   '/api': {
-        target: process.env.VITE_API_BASE_URL,
+      '/api': {
+        target: 'https://your-render-backend-url.com',
         changeOrigin: true,
         secure: false,
       },
